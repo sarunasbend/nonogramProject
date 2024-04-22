bac778cfbf4de055959969995695dd578ad2c11b
sarunasbend <s.b.seriouscontact@gmail.com>
2024-04-22 20:37:36 +0100

    Added BMP class which will be responsible for reading the .bmp file. As well as thedriver which is where the main function will

--- BMP.java
+++ BMP.java
@@ -1,3 +1,98 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+import java.io.*;
+
 public class BMP {
+    //essential parameters necessary for data parsing and translation
+    private String fileLocation;
+    private int width;
+    private int height;
+    private int bitsPerPixel;
+    private byte[][] unparsedData;
+    private int[] parsedData;
     
+    //constructor
+    public BMP(String fileLocation){
+        this.fileLocation = fileLocation;
+        setWidth();
+        setHeight();
+        setBitsPerPixel();
+    }
+
+    private void setWidth(){
+        //finds the width from the inputted BMP file by looking at the 18th - 21th bytes of the file
+        long widthBytePosition = 18;
+        try (RandomAccessFile bmp = new RandomAccessFile(this.fileLocation, "r")){
+            bmp.seek(widthBytePosition);
+            this.width = bmp.read();
+            bmp.seek(widthBytePosition + 1);
+            this.width = this.width + bmp.read();
+            bmp.seek(widthBytePosition + 2);
+            this.width = this.width + bmp.read();
+            bmp.seek(widthBytePosition + 3);
+            this.width = this.width + bmp.read();
+        } catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    private void setHeight(){
+        //finds the height from the inputted BMP file by looking at the 22nd - 25th bytes of the file  
+        long heightBytePosition = 22;
+        try (RandomAccessFile bmp = new RandomAccessFile(this.fileLocation, "r")){
+            bmp.seek(heightBytePosition);
+            this.height = bmp.read();
+            bmp.seek(heightBytePosition + 1);
+            this.height = this.height + bmp.read();
+            bmp.seek(heightBytePosition + 2);
+            this.height = this.height + bmp.read();
+            bmp.seek(heightBytePosition + 3);
+            this.height = this.height + bmp.read();
+        } catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    private void setBitsPerPixel(){
+        //finds the bits per pixel from the inputted BMP file by looking at the 28th - 29th bytes of the file 
+        long bitsPerPixelBytePosition = 28;
+        try (RandomAccessFile bmp = new RandomAccessFile(this.fileLocation, "r")){
+            bmp.seek(bitsPerPixelBytePosition);
+            this.bitsPerPixel = bmp.read();
+            bmp.seek(bitsPerPixelBytePosition + 1);
+            this.bitsPerPixel = this.bitsPerPixel + bmp.read();
+        } catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public int getWidth(){return this.width;}
+
+    public int getHeight(){return this.height;}
+
+    public int getBitsPerPixel(){return this.bitsPerPixel;}
+    
+    //debugging method, creates a txt file with all the hex values of each byte address
+    public void createTxt() throws IOException{
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream(this.fileLocation);
+            out = new FileOutputStream("output.txt");
+            int c;
+            while ((c = in.read()) != -1){
+                out.write(c);
+            }
+        } finally {
+            if (in != null){
+                in.close();
+            }
+            if (out != null){
+                out.close();
+            }
+        }
+    }
 }
---
+++ Driver.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Driver {
+    public static void main(String[] args) throws IOException{
+        BMP temp = new BMP("summer-project/2colour_elephant.bmp");
+        temp.createTxt();
+    }
+}
