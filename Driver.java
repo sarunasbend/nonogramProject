7561ea109443a2389d434c398c5bb1fc4f2458fa
sarunasbend <s.b.seriouscontact@gmail.com>
2024-05-14 15:06:34 +0100

    Working on the home page of the GUI, implemented the drop down menu as well as the button for selecting the puzzle. However, encountering an issue where I cannot close the home gui

--- Driver.java
+++ Driver.java
@@ -4,12 +4,11 @@ import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) throws IOException{
-        HomeGUI home = new HomeGUI();
-        while (!home.validPuzzle()){
-            ;
-        }
-
+        /*HomeGUI home = new HomeGUI();
+        while ((home.validPuzzle() == false)){
+        }*/
         BMP bmp = new BMP("puzzleImages/square.bmp"); //cannot do 16 bit-depths yet
+        //home.closeHomeGUI();
         Nonogram puzzle = new Nonogram(bmp, false);
         ArrayList<int[]> colourPalette = puzzle.getColourPalette();
         ColourButtons buttons = new ColourButtons(colourPalette, puzzle);
--- HomeGUI.java
+++ HomeGUI.java
@@ -1,6 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
+
 import java.io.File;
 import java.util.ArrayList;
 
@@ -18,6 +23,7 @@ public class HomeGUI extends JFrame {
         }
 
         public String[] getPuzzleNames(){
+            this.fileNames = new ArrayList<>();
             if (this.allFiles != null){
                 for (File name : this.allFiles){
                     if (name.isFile()){
@@ -40,9 +46,13 @@ public class HomeGUI extends JFrame {
     private JPanel mainPanel;
     private JComboBox<String> dropDownPanel;
     private FileReader files;
+    private JPanel temp;
+    private JButton playButton;
     
     public HomeGUI(){
         this.files = new FileReader();
+        initTemp();
+        initPlayButton();
         initDropDownMenu();
         initMainPanel();
         initMainFrame();
@@ -58,11 +68,13 @@ public class HomeGUI extends JFrame {
         setVisible(true);
 
         getContentPane().add(this.mainPanel);
+        getContentPane().add(this.temp);
+        getContentPane().add(this.playButton);
     }
 
     private void initMainPanel(){
         this.mainPanel = new JPanel(new BorderLayout());
-        this.mainPanel.setBounds(300,400,50,50);
+        this.mainPanel.setBounds(200,400,200,50);
         this.mainPanel.setBackground(new Color(255, 255, 0));
         this.mainPanel.add(this.dropDownPanel);
     }
@@ -71,7 +83,39 @@ public class HomeGUI extends JFrame {
         this.dropDownPanel = new JComboBox<>(this.files.getPuzzleNames());
     }
 
+    //TEMPORARY PANEL
+    private void initTemp(){
+        this.temp = new JPanel(new BorderLayout());
+        this.temp.setBounds(200, 150, 200, 200);
+        this.temp.setBackground(new Color(255, 0, 0));
+    }
+
+    private void initPlayButton(){
+        this.playButton = new JButton("PLAY");
+        this.playButton.setBackground(new Color(0,0,255));
+        this.playButton.setBounds(200, 500, 200, 100);
+        this.playButton.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event){
+                getDropDownMenuSelected();
+                setValidPuzzle();
+            }
+        });
+    }
+    private void setValidPuzzle(){
+        this.validPuzzle = true;
+    }
+
     public boolean validPuzzle(){
-        return false;
+        return this.validPuzzle;
+    }
+
+    //weird method type
+    public String getDropDownMenuSelected(){
+        System.out.println((String)(this.dropDownPanel.getSelectedItem()));
+        return (String)(this.dropDownPanel.getSelectedItem());
+    }
+
+    public void closeHomeGUI(){
+        dispose();
     }
 }
